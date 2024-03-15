af16cce2e577db21e8b72ed620ffaf061d1ecee2
Preetham Pemmasani <75422607+Preetham-ai@users.noreply.github.com>
2024-03-15 23:21:06 +0530

    Frontend Final

--- extension/src/components/header.jsx
+++ extension/src/components/header.jsx
@@ -1,35 +1,48 @@
 import React, { useState } from 'react';
 import './style.css';
-import logo from '../assets/faviconTransparent.png'; // Import the logo image
+import logo from '../assets/faviconTransparent.png';
 
 export default function Header() {
-  const [Text, setText] = useState(''); // Create a state for the text input
+    const [Text, setText] = useState('');
 
-  return (
-    <div>
-      <header>
-        <img src={logo} alt="GitGuru Logo" className="logo-small" />
-        <div className="gitguru-text">
-          Git Guru
+    function Explanation(){
+      // Explanation: Insert your backend code here guys
+        return "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi optio, velit quam vero vel dicta non repellat vitae aspernatur tempore odio in cumque, distinctio eos laudantium exercitationem eveniet eius debitis?"
+    }
+    function Summary(){
+      // Summary: Insert your backend code here guys
+      return "Summary text"
+    }
+    function Solutions(){
+      // Solutions: Insert your backend code here guys
+        return "Solutions text"
+    }
+    
+    return (
+        <div>
+            <header>
+                <img src={logo} alt="GitGuru Logo" className="logo-small" />
+                <div className="gitguru-text">
+                    Git Guru
+                </div>
+            </header>
+            <div className='wow'>
+                <div className="radio-inputs">
+                    <label className="radio">
+                        <input type="radio" name="radio" defaultChecked onClick={() => setText(Explanation())} />
+                        <span className="name">Explanation</span>
+                    </label>
+                    <label className="radio">
+                        <input type="radio" name="radio" onClick={() => setText(Summary())}/>
+                        <span className="name">Summary</span>
+                    </label>
+                    <label className="radio">
+                        <input type="radio" name="radio" onClick={() => setText(Solutions())} />
+                        <span className="name">Solutions</span>
+                    </label>
+                </div>
+                <p className="textBox">{Text}</p>
+            </div>
         </div>
-      </header>
-      <div className='wow'>
-        <div className="radio-inputs">
-          <label className="radio">
-            <input type="radio" name="radio" onClick={() => setText('Explanation')} />
-            <span className="name">Explanation</span>
-          </label>
-          <label className="radio">
-            <input type="radio" name="radio" defaultChecked />
-            <span className="name">Summary</span>
-          </label>
-          <label className="radio">
-            <input type="radio" name="radio" onClick={() => setText('Solutions')} />
-            <span className="name">Solutions</span>
-          </label>
-        </div>
-        <input type="text" value={Text} onChange={(e) => setText(e.target.value)} />
-      </div>
-    </div>
-  );
+    );
 }
